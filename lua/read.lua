b479921a613ae716e72934f135712866296d3872
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-05-27 12:00:24 -0300

    Implements read.all_buffer()
    This function reads all the contents of the current edited buffer to a
    lua string.

--- lua/read.lua
+++ lua/read.lua
@@ -1,5 +1,12 @@
 local read = {}
 
+---Read all content of current buffer to a lua string.
+---@return string
+function read.all_buffer()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    return table.concat(lines, "\n")
+end
+
 ---Read last saved version of current file to a lua string.
 ---@return nil
 function read.saved_file()
