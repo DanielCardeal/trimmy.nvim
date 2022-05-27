035c17732788c9f9d4631dd018815769d4661725
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-05-27 12:33:59 -0300

    Implements read.buffer_block()
    This function reads a block of lines from the current edited buffer.

--- lua/read.lua
+++ lua/read.lua
@@ -7,6 +7,24 @@ function read.all_buffer()
     return table.concat(lines, "\n")
 end
 
+---Gets a block of `block_size` lines starting at `line_start` from the current
+---buffer.
+---
+---Indexing is zero-based and delta has to be >= 0.
+---@param line_start number
+---@param block_size number
+---@return table
+function read.buffer_block(line_start, block_size)
+    block_size = (block_size >= 0) and block_size or 0
+    local line_end = line_start + block_size
+    return vim.api.nvim_buf_get_lines(
+        0,
+        line_start,
+        line_end,
+        false
+    )
+end
+
 ---Read last saved version of current file to a lua string.
 ---@return nil
 function read.saved_file()
