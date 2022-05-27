85351eab4ec2a3d722feaa804f60bfee4f93e9c2
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-05-27 11:40:53 -0300

    Add block trimming function

--- lua/trim_funcs.lua
+++ lua/trim_funcs.lua
@@ -8,4 +8,17 @@ function trim_funcs.trim_line(line)
     return line:gsub("%s+$", "")
 end
 
+---Remove trailing whitespace from a block of lines. A block of lines is a list
+---of lines
+---@param block table
+---@return table
+function trim_funcs.trim_block(block)
+    block = block or {}
+    local result_block = {}
+    for i, line in ipairs(block) do
+        result_block[i] = trim_funcs.trim_line(line)
+    end
+    return result_block
+end
+
 return trim_funcs
