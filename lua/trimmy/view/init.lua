f73bce940aeb6d709578fb9ebd612df647ef23d6
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-05-31 23:18:03 -0300

    Implement smart toggling of trimOnSave

--- lua/trimmy/view/init.lua
+++ lua/trimmy/view/init.lua
@@ -14,20 +14,37 @@ function view.init(trimBuffer, userConfig)
         {}
     )
 
-    -- Trim on save
-    if not userConfig.trimOnSave then
-        return
+    -- Setup trimOnSave toggling
+    local enabled = false
+    local autocmdId = nil
+    local trimmyGroup = vim.api.nvim_create_augroup("TrimmyGroup", {})
+    local toggleTrimOnSave = function()
+        if not enabled then
+            autocmdId = vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+                group = trimmyGroup,
+                pattern = userConfig.pattern,
+                callback = function()
+                    local bufnr = vim.call('bufnr')
+                    trimBuffer(bufnr)
+                end
+            })
+            enabled = true
+        else
+            vim.api.nvim_del_autocmd(autocmdId)
+            enabled = false
+        end
     end
 
-    local trimmyGroup = vim.api.nvim_create_augroup("TrimmyGroup", {})
-    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-        group = trimmyGroup,
-        pattern = userConfig.pattern,
-        callback = function()
-            local bufnr = vim.call('bufnr')
-            trimBuffer(bufnr)
-        end
-    })
+    vim.api.nvim_create_user_command(
+        "TrimmyToggleTrimOnSave",
+        toggleTrimOnSave,
+        {}
+    )
+
+    -- Initialize with trimOnSave enabled
+    if userConfig.trimOnSave then
+        toggleTrimOnSave()
+    end
 end
 
 return view
