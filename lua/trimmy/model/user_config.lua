d3007c6fb794fdb4b0b6ef881644250ff2b3d4e8
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-05-31 22:37:21 -0300

    Validates user passed configuration arguments

--- lua/trimmy/model/user_config.lua
+++ lua/trimmy/model/user_config.lua
@@ -7,12 +7,37 @@ local UserConfig = {
 }
 
 --[[###########################################################################
+                           PRIVATE FUNCTIONS
+##############################################################################]]
+
+---Checks if user options table contains only valid configuration keys. If a
+---invalid configuration key is found, return it, otherwise, returns `nil`.
+---@param opts table
+---@return string?
+local function validateOptKeys(opts)
+    for opt, _ in pairs(opts) do
+        if not UserConfig[opt] then
+            return opt
+        end
+    end
+end
+
+--[[###########################################################################
                             PUBLIC FUNCTIONS
 ##############################################################################]]
 ---Create UserConfig object based on user passed options.
 ---@param opts table
 ---@return UserConfig
 function UserConfig:from(opts)
+    local badOpt = validateOptKeys(opts)
+    if badOpt then
+        local err = string.format(
+            "Trimmy: Invalid option '%s'. Available options are 'trimOnSave' and 'pattern'.",
+            badOpt
+        )
+        error(err)
+    end
+
     local obj = opts or {}
     setmetatable(obj, { __index = self })
     return obj
